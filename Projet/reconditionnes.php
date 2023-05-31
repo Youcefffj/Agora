cd841d17d0403a7043b783bff823f7cc6f7639a4
Youcefff <133585996+Youcefffj@users.noreply.github.com>
2023-05-31 18:44:48 +0200

    Add files via upload

---
+++ Projet/reconditionnes.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Agora Francia - Reconditionnés</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="Website icon" type="png" href="images/logo.png">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="wrapper">
+        <header>
+            <h1>Agora Francia</h1>
+            <img src="images/logo.png" alt="logo" class="logo">
+        </header>
+
+        <nav>
+            <ul class="list">
+                <li><a style="color:black;text-decoration:none" href="index.php">Accueil</a></li>
+                <li><a style="color:black;text-decoration:none" href="produits.php">Tout parcourir</a></li>
+                <li><a style="color:black;text-decoration:none" href="notifications.php">Notifications</a></li>
+                <li><a style="color:black;text-decoration:none" href="#">Panier</a></li>
+                <li><a style="color:black;text-decoration:none" href="login.php">Votre Compte</a></li>
+            </ul>
+        </nav>
+
+        <div class="flash">
+            <h2>Produits reconditionnés</h2>
+        </div>
+
+        <main>
+            <div class="container">
+                <div class="card">
+                    <img src="images/produit_reconditionne1.jpg" class="image1">
+                    <p>Produit reconditionné 1</p>
+                </div>
+
+                <div class="card">
+                    <img src="images/produit_reconditionne2.jpg" class="image1">
+                    <p>Produit reconditionné 2</p>
+                </div>
+
+                <div class="card">
+                    <img src="images/produit_reconditionne3.jpg" class="image1">
+                    <p>Produit reconditionné 3</p>
+                </div>
+            </div>
+        </main>
+
+        <footer>
+            <h><a href="AboutUs.php">À propos de nous</a> | Crédits | Ce que tu veux | Blablabla</h>
+        </footer>
+    </div>
+</body>
+</html>
