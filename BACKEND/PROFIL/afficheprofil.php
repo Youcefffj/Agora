265a1eaa10f68d31e4ce7a30c8d3874d9c2c1b49
EXY03 <119684761+EXY03@users.noreply.github.com>
2023-06-02 00:58:23 +0200

    HAHAHAHA CA AFFICHE TOUT ALLEZ BOUGEZ VOTRE CUL LE FRONT

--- BACKEND/PROFIL/afficheprofil.php
+++ BACKEND/PROFIL/afficheprofil.php
@@ -2,16 +2,40 @@
 
 session_start();
 include "../Connection.php";
+include "../Controller.php";
 
 echo $_SESSION['user'] ;
 
 $profil = $_SESSION['user'];
 
-function ShowProfile($profil){
+function ShowProfile(){
 
-    $prepare = Connection::$db->prepare("SELECT photo, pseudo, `status`, nom, prenom, email, adr FROM item");
-    $prepare->execute(array());
-    $result = $prepare->fetchAll();
+  $prepare = Connection::$db->prepare("SELECT * FROM user WHERE id_user = ?");
+  $prepare->execute(array($_SESSION['user']));
+  $show = $prepare->fetch();
+  echo $show['pseudo'];
+  echo $show['email'];
+  echo $show['nom'];
+  echo $show['prenom'];
+  //echo $show['image'];
+
+  $prepare = Connection::$db->prepare("SELECT * FROM adresse WHERE id_adr = ?");
+  $prepare->execute(array($show['id_adr']));
+  $showadr = $prepare->fetch();
+
+  echo $showadr['adresse_ligne1'];
+  echo $showadr['adresse_ligne2'];
+  echo $showadr['ville'];
+  echo $showadr['code_postal'];
+  echo $showadr['pays'];
+  echo $show['photo']
+  ?>
+
+<figure>
+  <img src="<?=$show['photo'];?>" class="image1">
+</figure>
+
+<?php
 
 }
 
